08479abd0640fbba9d85939680229758896f4fd0
Yug-Gupta <yuggupta905@gmail.com>
2026-01-06 20:06:53 +0530

    Daily Java Practice

---
+++ JavaScript/script.js
@@ -0,0 +1,29 @@
+var a = 10
+console.log(a)
+
+console.log("Hello World");
+
+console.warn("This is warning")
+
+console.error("This is an error")
+
+// alert("This is an alert message")
+
+// prompt("Type your name: ")
+
+
+var n = "Pappu"
+n = "Ravi"
+console.log(n)
+
+let m = "Kittu"
+m = "Bittu"
+console.log(m)
+
+const pi = 3.14
+// pi = 3.1456  THiS WILL GIVE ERROR
+console.log(pi)
+
+var y;
+console.log(y);  
+
