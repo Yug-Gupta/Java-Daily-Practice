5c7b2d7f2e661d2ebe9a4936f1c008aaa0bd5601
Yug-Gupta <yuggupta905@gmail.com>
2026-01-06 23:55:29 +0530

    Daily Java Practice

---
+++ JavaScript/script3.js
@@ -0,0 +1,25 @@
+// var a
+// console.log(a)  // Undefined
+
+// console.log(b)  // Not-defined
+
+var arr = [1,2,3]
+console.log(arr[6])  // In JS there is no null pointer 
+var fun = ()=>{
+    
+} 
+var brr = [1,"Yug","34",[],function(){}]
+console.log(brr)
+
+console.log(brr[1])
+console.log(brr[3])
+console.log(brr[4])
+
+// Syntax to make an object-
+/*
+--> var obj = {}
+--> var obj1 = new object()
+*/ 
+
+
+
