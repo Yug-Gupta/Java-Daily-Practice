4353f9a21e2480b791c85ce275ef777a94436153
Yug-Gupta <yuggupta905@gmail.com>
2026-01-08 19:12:31 +0530

    Daily Java Practice

---
+++ JavaScript/script5.js
@@ -0,0 +1,34 @@
+/*
+What is DOM?
+-> DOM stands for Document Object Model
+
+It's a way for web browser to represent and control HTML tags
+
+But why we need DOM ?
+
+-> In html we don't have control over tag like we want to to show or hide some detail
+on click of a button
+we are not able do it with html so we need to take help from DOM.
+
+
+what is DOM manipulation?
+
+->page me kuch bhi change krna ya manipulate krna use kam DOM manipulation kahte ha.
+
+
+access => reach => selection => change
+
+How to access element ?
+
+1. selection of element
+    document.getElementById
+    document.getElementsByTagName
+    document.getElementsByClassName
+    document.querySelector('button')
+    ,document.querySelector('#btn")
+    ,document/querySelector('.btn);
+    document.querySelector('h1)
+*/
+
+window.console.log("Yug")
+
