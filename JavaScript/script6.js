2b184021b1e82ce266fb7805734a123bd836898a
Yug-Gupta <yuggupta905@gmail.com>
2026-01-09 13:41:43 +0530

    Daily Java Practice

--- JavaScript/script6.js
+++ JavaScript/script6.js
@@ -24,6 +24,8 @@ var but = document.createElement("button");
 but.textContent="Start";
 but.classList.add("buck");
 
+
+// MouseEvent->
 document.querySelector("body").appendChild(but);
 // but.remove();  //remove the button
 but.addEventListener('click',function(){
@@ -38,6 +40,8 @@ but.addEventListener('click',function(){
 //     console.log("This is mouseout event")
 // })
 
+
+// KeyboardEvent->
 // but.addEventListener("keydown",()=>{
 //     console.log("This is keydown event")
 // })
@@ -47,4 +51,48 @@ but.addEventListener('click',function(){
 // })
 
 
+// Form and input Event->
+let box = document.createElement("input");
+box.textContent="Hello";
+box.classList.add("text");
 
+document.querySelector("body").appendChild(box)
+
+
+box.addEventListener("input",()=>{
+    console.log("You input in the box")
+})
+
+box.addEventListener("change",()=>{
+    console.log("Value change")
+})
+
+// --------------------------------------------------------
+/*In HTML -
+<form id="myForm">
+    <input type="text">
+    <button type="submit">Submit</button>
+</form>
+
+-> The below code you can see that this is the conversion of this HTML code-
+*/
+let form = document.createElement("form")
+form.id="myform"
+
+let input = document.createElement("input")
+input.type = "text"
+
+let button = document.createElement("button")
+button.type ="Submit"
+button.innerText="Submit"
+
+form.appendChild(input)
+form.appendChild(button)
+document.querySelector("body").appendChild(form)
+
+form.addEventListener("submit",(e)=>{
+    e.preventDefault()  // Stops page reload
+    console.log("User submit this")
+})
+
+//----------------------------------------------------
