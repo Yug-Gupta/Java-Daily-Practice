b064059535c53e575cca2438c529b5241dd892a5
Yug-Gupta <yuggupta905@gmail.com>
2026-01-10 00:04:04 +0530

    Daily Java Practice

---
+++ JavaScript2/Variables/script1.js
@@ -0,0 +1,37 @@
+let shopname = "Gupta Stationary";
+let customerNum = 10;
+
+console.log(shopname);
+customerNum = 20;
+console.log(customerNum);
+
+/*
+In JS there are three variables that we use-
+
+1. let 
+2. var ( this is and old way representation)
+3. const ( this use when you required fix value)
+
+*/
+
+const address = "Jhinjhak Bada chauraha";
+console.log(address);
+
+var name = "Yug";
+console.log(name);
+name = "Pream";
+console.log(name);
+
+// NOTE-> Diff between let and var-
+
+let chutiya = "chutiya banoge";
+console.log(chutiya);
+// let chutiya = "chutya ho tum";  //let mai tum ek hi variable ka use do bar nhi kar sakte ho
+
+var buddhu = "buddu banjaoge";
+console.log(buddhu); 
+var buddhu = "buddhu ho tum";   // lakin var mai hum use ar sakte hai issi leye yeh old way hai aur latetst JS mai use bhi nhi hota hai.
+console.log(buddhu);
+
+
+
