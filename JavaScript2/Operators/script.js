15b3bbbb0550a2f280d51f1e07805dc42c2d2211
Yug-Gupta <yuggupta905@gmail.com>
2026-01-10 00:56:02 +0530

    Daily Java Practice

---
+++ JavaScript2/Operators/script.js
@@ -0,0 +1,24 @@
+/*
+Operators->
+1. Arithmatic Operators -> +, -, *, /, %, **
+2. Assignment Operators -> =, *=, -=, +=, /=, %=
+3. Comparison Operators -> ==, ===, !=, !==, <=, >=
+4. Logical Operators -> && , ||
+5. Unary Opeartors -> typeof , !, ++ , --
+6. Ternary Operators -> ? true : false
+7. BitWise Operators -> &
+8. String Concatenation -> +, +=
+*/
+
+let a = 5;
+let b = 6;
+console.log(a**b); //exponent => 5^6
+
+console.log(10 == "10");  // == not check the type of datatype
+console.log(10 === "10");  // === check the type of datatype
+
+
+// Imp cheez- ( `` )
+let fname = "Yug";
+let s = `Hello ${fname} Welcome to the CFS`;  //( `` )
+console.log(s);
