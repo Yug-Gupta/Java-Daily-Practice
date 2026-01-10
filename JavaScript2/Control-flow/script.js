01f7e2c0faba86b21de88081c3c20741d2c902be
Yug-Gupta <yuggupta905@gmail.com>
2026-01-10 20:57:06 +0530

    Daily Java Practice

---
+++ JavaScript2/Control-flow/script.js
@@ -0,0 +1,51 @@
+/*
+Operators->
+*/
+
+/*
+Control-flow->
+1. make decision : if,else,switch
+2. repeat : for , while , do while, for each
+3. jump : break,continue,return
+*/ 
+
+const age = 26;
+if(age > 18){
+    console.log("You are allowed");
+}else if(age > 60){
+    console.log("You are not allowed to watch");
+}else{
+    console.log("You are kid so not allowed");
+}
+
+
+const day = 2;
+switch(day)
+{
+    case 0:
+        console.log("Today Monday");
+        break;
+    case 1:
+        console.log("Tuesday");
+        break;
+    case 2:
+        console.log("Wednesday");
+        break;
+    case 3: 
+        console.log("Thrusday");
+        break;
+    case 4:
+        console.log("Friday");
+        break;
+    case 5:
+        console.log("Saturday");
+        break;
+    case 6:
+        console.log("Sunday");
+        break;
+    default: 
+        console.log("Invalid input");
+}
+
+
+
