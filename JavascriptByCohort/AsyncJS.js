1b6f99d6f504194424a2cea817b83560750983ec
Yug-Gupta <yuggupta905@gmail.com>
2026-01-13 01:18:15 +0530

    Daily Java Practice

---
+++ JavascriptByCohort/AsyncJS.js
@@ -0,0 +1,18 @@
+// Synchronous code-
+function sum(a,b){
+    return parseInt(a)+parseInt(b);
+}
+let ans = sum("20",30);
+console.log(ans);
+
+function sumNo(n){
+    return n*(n+1)/2;
+}
+let res1 = sumNo(100);
+console.log(res1);
+let res2 = sumNo(1000);
+console.log(res2);
+let res3 = sumNo(10000);
+console.log(res3);
+
+
