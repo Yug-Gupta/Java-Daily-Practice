e5437f486a9f0ad0421d236ff63c323aedab4594
Yug-Gupta <yuggupta905@gmail.com>
2026-01-13 00:00:08 +0530

    Daily Java Practice

---
+++ JavascriptByCohort/script.js
@@ -0,0 +1,36 @@
+// Syntax of JS-
+let firstName = "John";
+const age = 25;
+var isStudent = true;
+console.log(firstName);
+console.log(age);
+console.log(isStudent);
+
+
+// Datatypes-
+var isStudent = "Yug";
+var isStudent = 45;
+console.log(isStudent);
+
+// let firstName = 45;  // It gives the error.
+// console.log(firstName);
+
+// In curr time do not use the var.
+
+let arr = [1,2,3,4];  // array
+console.log(arr[0]);
+
+// Functions-
+function greet(name){
+    return "Hello "+name;
+}
+let ans = greet("Yug");
+let ans1 = greet("Shyam");
+console.log(ans1);
+console.log(ans);
+
+function sum(a,b){
+    return a+b;
+}
+let x = sum(4,64);
+console.log(x);
