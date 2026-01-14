1cf66526a8582808ecee971127b8679cd30cd12a
Yug-Gupta <yuggupta905@gmail.com>
2026-01-15 00:06:20 +0530

    Daily Java Practice

--- JavascriptByCohort/Promises.js
+++ JavascriptByCohort/Promises.js
@@ -9,11 +9,17 @@ class Rectangle{
         return this.length*this.widht;    
     }
     paint(){
-        console.log(`Painting with color ${this.color}`);
+        console.log(`Painting with color ${this.color}`);  // This is string concatenation.
     }
 }
 
 const reac = new Rectangle(2,3,"red");
+const reac2 = new Rectangle(10,23,"black");
 const area = reac.area();
+const area2 = reac2.area();
+reac.paint();
 console.log(area);
+reac2.paint();
+console.log(area2);
+
 
