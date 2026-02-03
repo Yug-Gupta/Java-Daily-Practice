65d15de0a26f66110b085ee1aa2e6b282bb0016e
Yug-Gupta <yuggupta905@gmail.com>
2026-02-04 01:15:56 +0530

    Daily Java Practice

---
+++ React/index.js
@@ -0,0 +1,12 @@
+// JSON -> JavaScript Object Notation
+function multiply(a,b){
+    return a*b;
+}
+
+let value = multiply(54,12);
+console.log(value);
+
+const path = require("path");
+console.log(__dirname);
+console.log(__dirname+"/index.js");
+
---
+++ React/index.mjs
@@ -0,0 +1,5 @@
+import chalk from "chalk";  // modern import syntax
+
+console.log(chalk.blue('Hello World'));
+console.log(chalk.bold.red('This is an error message'));
+console.log(chalk.green.underline('THis is a success message'));
