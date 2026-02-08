e22a9c39574d732adbef722cb411db4ab9468ace
Yug-Gupta <yuggupta905@gmail.com>
2026-02-08 15:31:10 +0530

    Daily Java Practice

--- Cohort Web Dev/Week-4-TODO-APP/index.js
+++ Cohort Web Dev/Week-4-TODO-APP/index.js
@@ -1,2 +1,15 @@
 const express = require("express");
 
+const app = express();
+
+
+//route handlers-
+app.get('/' ,function (req,res){
+    res.send('Hello World');
+})
+
+app.get('/asd' ,function (req,res){
+    res.send('Hello World from the asd point');
+})
+
+app.listen(3001);
