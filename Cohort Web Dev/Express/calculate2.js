b7ac0c711e8542ce8b85bf4bb358207fb5ec6024
Yug-Gupta <yuggupta905@gmail.com>
2026-02-09 00:36:51 +0530

    Daily Java Practice

--- Cohort Web Dev/Express/calculate2.js
+++ Cohort Web Dev/Express/calculate2.js
@@ -3,14 +3,15 @@ const express = require("express");
 const app = express();
 
 function calculateSum(a,b){
-    return a+b;
+    let sum = a+b;
+    return sum;
 }
 
 app.get('/',function (req,res){
-    let a = req.query.a;
-    let b = req.query.b;
-    let ans = calculateSum(a,b);
-    res.send(ans);
+    const a = req.query.a;
+    const b = req.query.b;
+    const ans = calculateSum(a,b);
+    res.send(ans.toString());
 })
 
 app.listen(8000,()=>{
