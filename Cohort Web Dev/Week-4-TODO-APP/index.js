514359abbb3d7cf8fd0a09541e9cee1d24bb6043
Yug-Gupta <yuggupta905@gmail.com>
2026-02-08 15:37:10 +0530

    Daily Java Practice

--- Cohort Web Dev/Week-4-TODO-APP/index.js
+++ Cohort Web Dev/Week-4-TODO-APP/index.js
@@ -5,11 +5,17 @@ const app = express();
 
 //route handlers-
 app.get('/' ,function (req,res){
+    // req -> this is request objext and this is the inbuilt argument of express library.
+    // res -> this is response object and this is also the inbuilt argument of express library
     res.send('Hello World');
 })
 
 app.get('/asd' ,function (req,res){
-    res.send('Hello World from the asd point');
+    res.send('<b>Hi there this from the asd endpoint</b>');
+})
+
+app.post('/',function (req,res){
+    res.send('Hello World from the post endpoint');
 })
 
 app.listen(3001);
