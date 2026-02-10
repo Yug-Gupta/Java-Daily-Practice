e7d8a1c41ff3145b92e629d0aeb5ef7ff64b222f
Yug-Gupta <yuggupta905@gmail.com>
2026-02-10 23:58:37 +0530

    Daily Java Practice

--- Cohort Web Dev/Express/index2.js
+++ Cohort Web Dev/Express/index2.js
@@ -1,15 +1,22 @@
 const express = require("express");
-
+const fs = require("fs");
+const path = require("path");
 const app = express();
 
 
-// This ( /:fileName ) -> Whatever you can write in the instead of fileName this will defualt get this route. )
-app.get("/files/:fileName",function(req,res){
+// This ( /:fileName ) -> Whatever you can write in the instead of fileName this will default get this route. )
+app.get("/:fileName",function(req,res){
     const name = req.params.fileName;
-    console.log(name);
-    res.json({});
+    const filePath = path.join(__dirname,name);
+    console.log(`Your file name is : ${name}`);
+    fs.readFile(filePath,"utf-8",function (err,data){
+        const fixedData = data.replace(/\r\n/g, "\n");
+        res.json({
+            "data" : fixedData
+        });
+    })
 });
 
 app.listen(3000,()=>{
     console.log("Server is running in http://localhost:3000");
-});
+}); 
