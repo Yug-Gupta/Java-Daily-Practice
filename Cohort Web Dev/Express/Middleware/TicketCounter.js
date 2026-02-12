c51b6edd240a14bf1e9546114805289c3bbd48ea
Yug-Gupta <yuggupta905@gmail.com>
2026-02-13 00:05:22 +0530

    Daily Java Practice

---
+++ Cohort Web Dev/Express/Middleware/TicketCounter.js
@@ -0,0 +1,35 @@
+const express = require("express");
+
+const app = express();
+
+
+function isEligible(age){
+    if(age >= 14){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+function isOldEnough(req,res,next){
+    const age = req.query.age;
+    if(age >= 14){
+        next();
+    }else{
+        res.json({
+            msg : "Sorry you are not eligible"
+        })
+    }
+}
+
+app.use(isOldEnough);
+
+app.get("/ride1",function(req,res){
+    res.json({
+        msg : "You are eligible for the ride 1"
+    });
+})
+
+app.listen(3000,()=>{
+    console.log("Server is running on http://localhost:3000/ride1");
+});
