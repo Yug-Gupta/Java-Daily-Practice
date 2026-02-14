f1eb40bc2baa1620272412ada3fe88619883a325
Yug-Gupta <yuggupta905@gmail.com>
2026-02-14 16:14:55 +0530

    Daily Java Practice

---
+++ Cohort Web Dev/Express/Middleware/RateLimiter.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+
+let numberOfRequestForUser = {};
+setInterval(()=>{
+    numberOfRequestForUser = {};
+},1000);
+
+app.use(function(req,res,next){
+    const userId = req.headers["user-id"];
+    if(numberOfRequestForUser[userId]){
+        numberOfRequestForUser[userId]++;
+        if(numberOfRequestForUser[userId] > 5){
+            res.status(404).send("no enrty");
+        }else{
+            next();
+        }
+    }else{
+        numberOfRequestForUser[userId] = 1;
+        next();
+    }
+})
