3a33bb7822939f9b2456253488aa844d83b2f35a
Yug-Gupta <yuggupta905@gmail.com>
2026-02-15 23:18:35 +0530

    Daily Java Practice

--- Cohort Web Dev/Express/Create an http server/Assignment.js
+++ Cohort Web Dev/Express/Create an http server/Assignment.js
@@ -1,33 +1,33 @@
 const express = require("express");
 const app = express();
 
-app.get("/sum",function(req,res){
-    const a = Number(req.query.a);
-    const b = Number(req.query.b);
+app.get("/sum/:a/:b",function(req,res){
+    const a = Number(req.params.a);
+    const b = Number(req.params.b);
     res.json({
         Sum : a+b
     });
 });
 
-app.get("/multiply",function(req,res){
-    const a = req.query.a;
-    const b = req.query.b;
+app.get("/multiply/:a/:b",function(req,res){
+    const a = req.params.a;
+    const b = req.params.b;
     res.json({
         Multiply : a*b
     });
 });
 
-app.get("/divide",function(req,res){
-    const a = req.query.a;
-    const b = req.query.b;
+app.get("/divide/:a/:b",function(req,res){
+    const a = req.params.a;
+    const b = req.params.b;
     res.json({
         Divide : a/b
     });
 });
 
-app.get("/sub",function(req,res){
-    const a = req.query.a;
-    const b = req.query.b;
+app.get("/sub/:a/:b",function(req,res){
+    const a = req.params.a;
+    const b = req.params.b;
     res.json({
         Sub : a-b
     });
