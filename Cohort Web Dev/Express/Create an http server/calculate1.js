544cc5c13e35af532227db76bb6de56bd18774e9
Yug-Gupta <yuggupta905@gmail.com>
2026-02-15 22:34:55 +0530

    Daily Java Practice

--- Cohort Web Dev/Express/Create an http server/calculate1.js
+++ Cohort Web Dev/Express/Create an http server/calculate1.js
@@ -1,10 +1,7 @@
 const express = require("express");
 
 function calculateSum(n){
-    let sum = 0;
-    for(let i = 1;i<=n;i++){
-        sum += i;
-    }
+    let sum = (n*(n+1))/2;
     return sum;
 }
 
