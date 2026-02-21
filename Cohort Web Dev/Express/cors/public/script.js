53be49886936509621f30300f861a58a4360ce06
Yug-Gupta <yuggupta905@gmail.com>
2026-02-21 19:28:17 +0530

    Make some changes in the cors directory

--- Cohort Web Dev/Express/cors/public/script.js
+++ Cohort Web Dev/Express/cors/public/script.js
@@ -1,8 +1,7 @@
 const express = require("express");
 const cors = require("cors");
+
 const app = express();
-
-
 app.use(express.json());
 app.use(cors());
 
@@ -12,7 +11,7 @@ app.post("/sum",function(req,res){
     res.json({
         Sum : a+b
     });
-});
+})
 
 app.listen(3000,()=>{
     console.log("Server is running");
