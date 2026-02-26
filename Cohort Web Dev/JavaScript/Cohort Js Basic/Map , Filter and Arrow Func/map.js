bce1871e7077b0fc6644f432fb0e337ceca075f2
Yug-Gupta <yuggupta905@gmail.com>
2026-02-26 23:45:28 +0530

    Update the map file

--- Cohort Web Dev/JavaScript/Cohort Js Basic/Map , Filter and Arrow Func/map.js
+++ Cohort Web Dev/JavaScript/Cohort Js Basic/Map , Filter and Arrow Func/map.js
@@ -1,10 +1,13 @@
 // Convert this array - [1,2,3,4,5]
 // into a new array - [2,4,6,8,10]
 
-let arr = [1,2,3,4,5];
-let newArray = []
+const arr = [1,2,3,4,5];
 
-for(let i =0 ;i<arr.length;i++){
-    newArray.push(arr[i]*2);
-}
-console.log(newArray);
+const ans = arr.map(function(i){
+    return i*2;
+});
+console.log(ans);
+
+
+// Whenever you use to transform an array you use an map
+
