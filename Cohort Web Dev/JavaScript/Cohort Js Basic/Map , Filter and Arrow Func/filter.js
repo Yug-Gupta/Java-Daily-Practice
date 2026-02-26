3b02f9c26ee92863ca4709af23d3158d5ca2f147
Yug-Gupta <yuggupta905@gmail.com>
2026-02-26 23:57:42 +0530

    Update the filter.js file

---
+++ Cohort Web Dev/JavaScript/Cohort Js Basic/Map , Filter and Arrow Func/filter.js
@@ -0,0 +1,13 @@
+//filtering-
+// What is I tell u , given an input array , give me back all the even values from it
+
+const arr = [1,2,3,4,5];
+
+const ans = arr.filter((n) => {
+    if(n%2 == 0){
+        return true;
+    }else{
+        return false;
+    }
+});
+console.log(ans);
